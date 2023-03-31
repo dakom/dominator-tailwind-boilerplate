23bdab0df1821fe408aa8cbd907025bd33d33c68
David Komer <david.komer@gmail.com>
2023-03-31 17:27:13 +0300

    button text

--- src/page/profile/dom.rs
+++ src/page/profile/dom.rs
@@ -42,7 +42,7 @@ impl ProfilePage {
             .children([
                 html!("div", {
                     .class("mt-6")
-                    .child(render_submit_button("Go visit"))
+                    .child(render_submit_button("Go (via permalink url)"))
                 }),
             ])
         })
--- src/primitives/form.rs
+++ src/primitives/form.rs
@@ -30,20 +30,6 @@ pub fn render_submit_button(label: &str) -> Dom {
         .child(
             html!("span", {
                 .class(["absolute","left-0","inset-y-0","flex","items-center","pl-3"])
-                .child(
-                    svg!("svg", {
-                        .class(["h-5","w-5","text-indigo-500","group-hover:text-indigo-400","transition","ease-in-out","duration-150"])
-                        .attr("fill", "currentColor")
-                        .attr("viewBox", "0 0 20 20")
-                        .child(
-                            svg!("path", {
-                                .attr("fill-rule", "evenodd")
-                                .attr("d", "M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z")
-                                .attr("clip-rule", "evenodd")
-                            })
-                        )
-                    })
-                )
             })
         )
     })
